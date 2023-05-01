6333b0b389e3eb3b6e5cddc4fd08d5f1d982af06
Michael.Soltan <miclotfy@gmail.com>
2023-05-01 12:42:45 +0200

    Revert "Revert "SignIn for google and email""
    This reverts commit bbc9cc160cd47bbb1040af47ad0af328bb63b44a.

---
+++ lib/core/utils/image_constant.dart
@@ -0,0 +1,49 @@
+class ImageConstant {
+  static String imgDate = 'assets/images/img_date.svg';
+
+  static String imgVector = 'assets/images/img_vector.svg';
+
+  static String imgImage6 = 'assets/images/img_image6.png';
+
+  static String imgGroup29 = 'assets/images/img_group29.svg';
+
+  static String imgOutdoor = 'assets/images/img_outdoor.png';
+
+  static String imgClippathgroup = 'assets/images/img_clippathgroup.svg';
+
+  static String imgImage6153x169 = 'assets/images/img_image6_153x169.png';
+
+  static String imgUndrawcouplelovere3fw6 =
+      'assets/images/img_undrawcouplelovere3fw6.png';
+
+  static String imgGroup13 = 'assets/images/img_group13.svg';
+
+  static String img3626667removebgpreview =
+      'assets/images/img_3626667removebgpreview.png';
+
+  static String imgAddnew = 'assets/images/img_addnew.svg';
+
+  static String imgUndrawbibliophilerexarc =
+      'assets/images/img_undrawbibliophilerexarc.png';
+
+  static String imgPlus = 'assets/images/img_plus.svg';
+
+  static String imgVolume = 'assets/images/img_volume.svg';
+
+  static String imgMail = 'assets/images/img_mail.svg';
+
+  static String imgImage1 = 'assets/images/img_image1.png';
+
+  static String imgFavorite = 'assets/images/img_favorite.svg';
+
+  static String imgVectorGray100 = 'assets/images/img_vector_gray_100.svg';
+
+  static String imgVectorRed500 = 'assets/images/img_vector_red_500.svg';
+
+  static String imgEye = 'assets/images/img_eye.svg';
+
+  static String imgUndrawspreadloverev3cl =
+      'assets/images/img_undrawspreadloverev3cl.png';
+
+  static String imageNotFound = 'assets/images/image_not_found.png';
+}
